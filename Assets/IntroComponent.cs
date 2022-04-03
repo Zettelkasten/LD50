221f77198acfc4ad081f32f647e3023d3def5cd1
Frithjof Petrick <fmpetrick@gmail.com>
2022-04-03 22:02:07 +0200

    wip stuff

---
+++ Assets/IntroComponent.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroComponent : MonoBehaviour
+{
+    public GameObject[] charList;
+    public string[] charNames;
+
+    public string[] dialogue = new string[]
+    {
+        "Hello",
+        "What the fuck",
+        "Good bye"
+    };
+}
