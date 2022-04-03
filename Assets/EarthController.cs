dd68425273f91464e41d53bb071b324c585586ef
decosense <stimmts@gmx.net>
2022-04-03 17:19:54 +0200

    reset button and frithjof button

--- Assets/EarthController.cs
+++ Assets/EarthController.cs
@@ -227,11 +227,12 @@ public class EarthController : MonoBehaviour
         //var animator = CdAnimator.GetComponent<Animator>();
         //var clipinfo = animator.GetCurrentAnimatorClipInfo(0);
 
-
+        var thrusterfactor = thrusterSlot.upgradeScales[thrusterSlot.upgradeLevel-1];
         this.transform.localPosition = new Vector3(pos.x, pos.y, 0);
         this.thruster.transform.eulerAngles = new Vector3(0, 0, this.angle - 90);
+        this.flame.transform.localPosition = new Vector3(x: 0f, y: -0.45f * (thrusterfactor + 1)/2, 0);
         this.flame.transform.eulerAngles = new Vector3(0, 0, this.angle - 90);
-        this.flame.transform.localScale = new Vector3(0.7f * flamesize, -0.7f * flamesize, 1f);
+        this.flame.transform.localScale = new Vector3(0.75f * flamesize * thrusterfactor, -0.75f * flamesize * thrusterfactor, 1f);
         this.camera.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.camera.transform.position.z);
         this.foodCounterText.text = this.numFood + " Potatos";
         this.asteroidCounterText.text = this.numAsteroidsDodged + " dodged";
@@ -306,11 +307,16 @@ public class EarthController : MonoBehaviour
             angle -= angleVelocity[thrusterSlot.upgradeLevel - 1];
         }
 
-        if (Input.GetKey("f"))
+        if (Input.GetKeyDown("f"))
         {
             numFood += 10;
         }
 
+        if (Input.GetKeyDown("r"))
+        {
+            SceneManager.LoadScene("EndScene"); 
+        }
+        
         this.velo *= 0.9f;
         if (Input.GetKey("w"))
         {
@@ -363,7 +369,6 @@ public class EarthController : MonoBehaviour
             }
 
             CdAnimator.speed = regenerationSpeed;
-            Debug.Log("regenerationSpeed" + regenerationSpeed);
             cdAnimation.SetActive(true);
             var animator = CdAnimator.GetComponent<Animator>();
             var clipinfo = animator.GetCurrentAnimatorClipInfo(0);
