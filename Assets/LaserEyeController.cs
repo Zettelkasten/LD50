7b4ab4df9a4753945bbddba7296b50c8efd65323
Frithjof Petrick <fmpetrick@gmail.com>
2022-04-03 20:45:19 +0200

    build stuff

--- Assets/LaserEyeController.cs
+++ Assets/LaserEyeController.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.UI;
-using UnityEditor.UIElements;
 using UnityEngine;
 
 public class LaserEyeController : MonoBehaviour
