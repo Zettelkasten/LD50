36ac682c8bd11f37de37e75d9848379151faafe2
decosense <stimmts@gmx.net>
2022-04-04 11:45:04 +0200

    critical asteroid marker

--- Assets/AsteroidController.cs
+++ Assets/AsteroidController.cs
@@ -11,10 +11,22 @@ public class AsteroidController : MonoBehaviour
     public float angle;  // in deg
     public Vector2 velo;
     public GameObject schweif;
+    public float criticalSpeed = 0.15f;
 
     private void Start()
     {
         this.angle = Random.value * 360;
+        
+        var halo = (Behaviour)this.GetComponent("Halo");
+        var dist_norm = Mathf.Sqrt(velo.SqrMagnitude());
+        if (dist_norm > criticalSpeed)
+        {
+            halo.enabled = true;
+        }
+        else
+        {
+            halo.enabled = false;
+        }
     }
 
     void Update()
@@ -22,6 +34,9 @@ public class AsteroidController : MonoBehaviour
         this.transform.localPosition = new Vector3(pos.x, pos.y, 0);
         this.transform.eulerAngles = new Vector3(0, 0, this.angle);
         this.schweif.transform.eulerAngles = new Vector3(0, 0, -Vector2.SignedAngle(velo, Vector2.right));
+        
+        
+        
     }
 
     private void FixedUpdate()
