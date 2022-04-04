95aa1dc6c607f119c80e88253c509570324ed0af
decosense <stimmts@gmx.net>
2022-04-04 20:23:39 +0200

    explosion

--- Assets/ContinentController.cs
+++ Assets/ContinentController.cs
@@ -27,11 +27,11 @@ public class ContinentController : MonoBehaviour
         if (i_continent < 4)
         {
             pos += new Vector2(spawn_x[i_continent], spawn_y[i_continent]);
-            velo = (new Vector2(spawn_x[i_continent], spawn_y[i_continent]) + ((Vector2) Random.onUnitSphere)) * 0.06f;
+            velo = (new Vector2(spawn_x[i_continent], spawn_y[i_continent]) + ((Vector2) Random.onUnitSphere)) * 0.06f * Random.value;
         }else if (i_continent == 4)
         {
-            pos += (Vector2) Random.onUnitSphere * 0.3f;
-            velo = (Vector2) Random.onUnitSphere * 0.12f;
+            pos += (Vector2) Random.onUnitSphere * 0.4f* Random.value;
+            velo = (Vector2) Random.onUnitSphere * 0.12f* Random.value;
             
         }
         angleVelo = (float) Random.value * 0.9f;
